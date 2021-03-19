f959c2a6b60eb72b9f086ab7c0bbf31ccef706da
PabloRamirezMartinez <slashblo@gmail.com>
2021-03-19 11:50:13 -0600

    programa finalizado

--- app.js
+++ app.js
@@ -35,6 +35,21 @@ document.getElementById("boton1").addEventListener("click", function() {
         var j = parseInt(j);
         var w = parseInt(w);
         var z = parseInt(z);
+        // asignamos una variable al valor de cada celda.
+
+        var x2 = document.getElementById("tabla1").rows[1].cells[0];
+        var r2 = document.getElementById("tabla1").rows[1].cells[1];
+        var y2 = document.getElementById("tabla1").rows[1].cells[2];
+        var s2 = document.getElementById("tabla1").rows[1].cells[3];
+        var l2 = document.getElementById("tabla1").rows[1].cells[4];
+        var p2 = document.getElementById("tabla1").rows[1].cells[5];
+        var t2 = document.getElementById("tabla2").rows[1].cells[0];
+        var g2 = document.getElementById("tabla2").rows[1].cells[1];
+        var a2 = document.getElementById("tabla2").rows[1].cells[2];
+        var j2 = document.getElementById("tabla2").rows[1].cells[3];
+        var w2 = document.getElementById("tabla2").rows[1].cells[4];
+        var z2 = document.getElementById("tabla2").rows[1].cells[5];
+
       
    
 var operacion = document.getElementById("operacion").value;
@@ -48,17 +63,8 @@ j = t+l;
 z = p*r+y;
 y = y+z;
 y = y+1;
-console.log(x)
-console.log(w)
-console.log(t)
-console.log(j)
-console.log(z)
-console.log(y)
-console.log(x2);
-document.getElementById("X2").value = x;
 }
-
-    //operaciones ejercicio 2;
+   //operaciones ejercicio 2;
 if (operacion ==2 ){
     x = x+a-w;
     s = x-j*z;
@@ -69,10 +75,22 @@ if (operacion ==2 ){
     w = w+5;
     j = j+z/x+2;
 }
+
     //resultados finales
 
 
-
+    x2.innerHTML = x.toString();
+    r2.innerHTML = r.toString();
+    y2.innerHTML = y.toString();
+    s2.innerHTML = s.toString();
+    l2.innerHTML = l.toString();
+    p2.innerHTML = p.toString();
+    t2.innerHTML = t.toString();
+    g2.innerHTML = g.toString();
+    a2.innerHTML = a.toString();
+    j2.innerHTML = j.toString();
+    w2.innerHTML = w.toString();
+    z2.innerHTML = z.toString();
         });
 
 
